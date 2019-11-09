23765a1aabdc00c18a68b8c03dd609cec3f88c1e
willysantoso05 <willysantoso05@gmail.com>
2019-11-09 16:37:25 +0700

    Initiate files

---
+++ client/client.js
@@ -0,0 +1,4 @@
+//Tugas Besar II Jarkom
+const PORT = 9000;
+
+var wsClient = new WebSocket(`ws://localhost:${PORT}/`);
---
+++ web-socket/web-socket.py
@@ -0,0 +1,27 @@
+#Tugas Besar II Jarkom
+
+'''
++-+-+-+-+-------+-+-------------+-------------------------------+
+|F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+|I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+|N|V|V|V|       |S|             |   (if payload len==126/127)   |
+| |1|2|3|       |K|             |                               |
++-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+|     Extended payload length continued, if payload len == 127  |
++ - - - - - - - - - - - - - - - +-------------------------------+
+|                               |Masking-key, if MASK set to 1  |
++-------------------------------+-------------------------------+
+| Masking-key (continued)       |          Payload Data         |
++-------------------------------- - - - - - - - - - - - - - - - +
+:                     Payload Data continued ...                :
++ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+|                     Payload Data continued ...                |
++---------------------------------------------------------------+
+'''
+
+OPCODE_CONTINUATION_FRAME = 0x0
+OPCODE_TEXT_FRAME         = 0x1
+OPCODE_BINARY_FRAME       = 0x2
+OPCODE_CLOSE_CONNECTION   = 0x8
+OPCODE_PING               = 0x9
+OPCODE_PONG               = 0xA
