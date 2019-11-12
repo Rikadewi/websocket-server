a9f10846234a36f4ac417f3f74bc3690309ac692
lukaskurnia <lukasjonathan99@gmail.com>
2019-11-12 15:59:53 +0700

    finish

--- webSocketHandler.py
+++ webSocketHandler.py
@@ -20,6 +20,7 @@ import socketserver
 |                     Payload Data continued ...                |
 +---------------------------------------------------------------+
 '''
+
 GUID = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
 
 OPCODE_CONTINUATION_FRAME = 0x0
